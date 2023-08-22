b405dd8d26802623e409507503d33698ed1a9ec1
koskeljs <jesse.s.koskela@student.jyu.fi>
2023-08-22 20:45:29 +0300

    unicafe step2

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -4,6 +4,7 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const total = good + neutral + bad
 
   return (
     <div>
@@ -15,6 +16,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {(good - bad)/(total)}</p>
+      <p>positive {good/total}</p>
     </div>
   )
 }
