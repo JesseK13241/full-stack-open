a01243ef2ddc462d36659ab874f4923cd23b6b56
koskeljs <jesse.s.koskela@student.jyu.fi>
2023-08-22 20:51:28 +0300

    unicafe step3

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,24 +1,32 @@
 import { useState } from 'react'
 
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad
+  return (
+    <>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {(good - bad)/(total) || 0}</p>
+      <p>positive {good/total || 0}</p>
+    </>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const total = good + neutral + bad
-
+  
   return (
     <div>
       <h1>give feedback</h1>
       <button onClick={() => setGood(good + 1)}>good</button>
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
-      <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {total}</p>
-      <p>average {(good - bad)/(total)}</p>
-      <p>positive {good/total}</p>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
