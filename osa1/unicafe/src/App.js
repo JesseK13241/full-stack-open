906b4735910a4b98ab8ba8fe466da9002d6e2aff
koskeljs <jesse.s.koskela@student.jyu.fi>
2023-08-22 21:11:31 +0300

    unicafe step6

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -5,7 +5,15 @@ const Button = ({ text, handleClick }) => {
 }
 
 const StatisticLine = ({ text, value }) => {
-  return <p>{text} {value}</p>
+  return (
+    <tr>
+      <td>
+        {text}
+      </td>
+      <td>
+        {value}
+      </td>
+    </tr>)
 }
 
 const Statistics = ({ good, neutral, bad }) => {
@@ -14,12 +22,16 @@ const Statistics = ({ good, neutral, bad }) => {
     return (
       <>
         <h1>statistics</h1>
-        <StatisticLine text="good" value={good}/>
-        <StatisticLine text="neutral" value={neutral}/>
-        <StatisticLine text="bad" value={bad}/>
-        <StatisticLine text="all" value={total}/>
-        <StatisticLine text="average" value={(good - bad)/total || 0}/>
-        <StatisticLine text="positive" value={good/total || 0}/>
+        <table>
+          <tbody>
+            <StatisticLine text="good" value={good}/>
+            <StatisticLine text="neutral" value={neutral}/>
+            <StatisticLine text="bad" value={bad}/>
+            <StatisticLine text="all" value={total}/>
+            <StatisticLine text="average" value={((good - bad)/total).toFixed(4)}/>
+            <StatisticLine text="positive" value={`${((good/total) * 100).toFixed(1)} %`}/>
+          </tbody>
+        </table>
       </>
     )
   }
