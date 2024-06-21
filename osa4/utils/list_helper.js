8b1e8393fcc0594ddae78db59a4d82db2dae5c47
JesseK13241 <138243431+JesseK13241@users.noreply.github.com>
2024-06-21 11:44:21 +0300

    4.4: apufunktioita ja yksikkötestejä, step2

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -1,7 +1,13 @@
-const dummy = (blogs) => {
-    return 1
+const totalLikes = (blogs) => {
+    const reducer = (sum, blog) => {
+        return sum + blog.likes
+    }
+    if (blogs.length === 0) { return 0 }
+    const total = blogs.reduce(reducer, 0)
+    if (isNaN(total)) { return 0 }
+    return total
 }
 
 module.exports = {
-    dummy
+    totalLikes
 }
