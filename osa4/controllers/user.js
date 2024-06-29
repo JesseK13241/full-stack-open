1bcc9b32252df135d15e5a1b837f767cbb2d24cb
JesseK13241 <138243431+JesseK13241@users.noreply.github.com>
2024-06-29 18:24:15 +0300

    4.16*: blogilistan laajennus, step4

--- osa4/controllers/user.js
+++ osa4/controllers/user.js
@@ -11,6 +11,14 @@ usersRouter.get("/", async (request, response) => {
 usersRouter.post("/", async (request, response) => {
     const { username, name, password } = request.body
 
+    if (!username || !password) {
+        return response.status(400).json({ error: "empty username or password" })
+    }
+
+    if (username.length < 3 || password.length < 3) {
+        return response.status(400).json({ error: "username and password must be longer than 3 characters" })
+    }
+
     const saltRounds = 10
     const passwordHash = await bcrypt.hash(password, saltRounds)
 
