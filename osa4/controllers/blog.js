267e5ed2b11ef5c378782a394a9f2e8642ac7e1d
JesseK13241 <138243431+JesseK13241@users.noreply.github.com>
2024-06-29 19:08:55 +0300

    4.18, 19, 20: blogilistan laajennus, step6, 7, 8

--- osa4/controllers/blog.js
+++ osa4/controllers/blog.js
@@ -1,15 +1,5 @@
 const blogsRouter = require("express").Router()
 const Blog = require("../models/blog")
-const User = require("../models/user")
-const jwt = require("jsonwebtoken")
-
-const getTokenFrom = request => {
-    const authorization = request.get("authorization")
-    if (authorization && authorization.startsWith("Bearer ")) {
-        return authorization.replace("Bearer ", "")
-    }
-    return null
-}
 
 blogsRouter.get("/", async (request, response) => {
     const blogs = await Blog.find({})
@@ -27,13 +17,7 @@ blogsRouter.get("/:id", async (request, response) => {
 
 blogsRouter.post("/", async (request, response) => {
     const body = request.body
-
-    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
-    if (!decodedToken.id) {
-        return response.status(401).json({ error: "token invalid" })
-    }
-
-    const user = await User.findById(decodedToken.id)
+    const user = request.user
 
     const blog = new Blog(body)
     blog.user = user._id
