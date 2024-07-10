0f5ce7b77887e582044ed76abfedbfac1faaaac0
JesseK13241 <138243431+JesseK13241@users.noreply.github.com>
2024-07-10 14:41:31 +0300

    6.9 paremmat anekdootit, step7

--- osa6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ osa6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -3,7 +3,13 @@ import { increaseVote } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = () => {
 
-    const anecdotes = useSelector(state => state)
+    const anecdotes = useSelector(state => state.anecdotes)
+    const filter = useSelector(state => state.filter)
+
+    const filteredAnecdotes = anecdotes.filter(anecdote =>
+        anecdote.content.toLowerCase().includes(filter.toLowerCase())
+    )
+
     const dispatch = useDispatch()
 
     const vote = (id) => {
@@ -14,7 +20,7 @@ const AnecdoteList = () => {
     return (
         <>
             <h2>Anecdotes</h2>
-            {anecdotes.sort((a, b) => a.votes - b.votes).reverse().map(anecdote =>
+            {filteredAnecdotes.sort((a, b) => a.votes - b.votes).reverse().map(anecdote =>
                 <div key={anecdote.id}>
                     <div>
                         {anecdote.content}
