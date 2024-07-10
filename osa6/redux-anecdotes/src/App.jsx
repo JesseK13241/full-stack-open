418009658750a8bcb9bb8dded482d7a801db95ee
JesseK13241 <138243431+JesseK13241@users.noreply.github.com>
2024-07-10 14:01:29 +0300

    6.8: anekdootit, step6

--- osa6/redux-anecdotes/src/App.jsx
+++ osa6/redux-anecdotes/src/App.jsx
@@ -1,30 +1,10 @@
-import { useSelector, useDispatch } from 'react-redux'
-import { increaseVote } from './reducers/anecdoteReducer'
-import AnecdoteForm from "./components/AnecdoteForm.jsx"
+import AnecdoteForm from "./components/AnecdoteForm"
+import AnecdoteList from "./components/AnecdoteList"
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
-  const dispatch = useDispatch()
-
-  const vote = (id) => {
-    console.log('Voting', id)
-    dispatch(increaseVote(id))
-  }
-
   return (
     <div>
-      <h2>Anecdotes</h2>
-      {anecdotes.sort((a, b) => a.votes - b.votes).reverse().map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
+      <AnecdoteList />
       <AnecdoteForm />
     </div>
   )
