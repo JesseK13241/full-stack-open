0e4a566688fd754f1a8de22b38f828a967542370
JesseK13241 <138243431+JesseK13241@users.noreply.github.com>
2024-07-23 04:13:24 +0300

    8.11: Kirjailijan syntymävuosi

--- osa8/frontend/src/components/Authors.jsx
+++ osa8/frontend/src/components/Authors.jsx
@@ -1,8 +1,24 @@
-import { useQuery } from '@apollo/client'
-import { ALL_AUTHORS } from "../queries"
+import { useQuery, useMutation } from '@apollo/client'
+import { ALL_AUTHORS, EDIT_AUTHOR } from "../queries"
 import PropTypes from 'prop-types'
+import { useState } from 'react'
 
 const Authors = (props) => {
+
+  const [authorNameToUpdate, setAuthorNameToUpdate] = useState("")
+  const [authorYearToUpdate, setAuthorYearToUpdate] = useState(0)
+  const [ editAuthor ] = useMutation(EDIT_AUTHOR, {refetchQueries: [ { query: ALL_AUTHORS } ]})
+
+  const handleAuthorUpdate = (event) => {
+    event.preventDefault()
+
+    console.log('updating author...')
+    console.log( { authorNameToUpdate, authorYearToUpdate })
+    editAuthor({  variables: { name: authorNameToUpdate, setBornTo: authorYearToUpdate } })
+
+    setAuthorNameToUpdate("")
+    setAuthorYearToUpdate(0)
+  }
 
   const result = useQuery(ALL_AUTHORS)
   if (!result.data || !props.show) {
@@ -30,6 +46,27 @@ const Authors = (props) => {
           ))}
         </tbody>
       </table>
+
+      <h2>Set birthyear</h2>
+      <form onSubmit={handleAuthorUpdate}>
+        <div>
+          name
+          <input
+            value={authorNameToUpdate}
+            onChange={({ target }) => setAuthorNameToUpdate(target.value)}
+          />
+        </div>
+        <div>
+          born
+          <input
+            required
+            type="number"
+            value={authorYearToUpdate ? authorYearToUpdate : ""}
+            onChange={({ target }) => setAuthorYearToUpdate(parseInt(target.value))}
+          />
+        </div>
+        <button type="submit">update author</button>
+      </form>
     </div>
   )
 }
--- osa8/frontend/src/queries.js
+++ osa8/frontend/src/queries.js
@@ -31,5 +31,12 @@ export const CREATE_BOOK = gql`
       published
       genres
     }
-  }
-`
+  }`
+
+export const EDIT_AUTHOR = gql`
+mutation editAuthor($name: String!, $setBornTo: Int!) {
+    editAuthor(name: $name, setBornTo: $setBornTo)  {
+      name
+      born
+    }
+  }`
