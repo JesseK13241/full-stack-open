1a8446ab6a3e8ae34e441f3ac768dfa4ce46f74c
JesseK13241 <138243431+JesseK13241@users.noreply.github.com>
2024-08-04 19:35:34 +0300

    Exercise 10.10: a platform-specific font

--- osa10/rate-repository-app/src/theme.js
+++ osa10/rate-repository-app/src/theme.js
@@ -1,15 +1,21 @@
+import { Platform } from "react-native";
+
 const theme = {
   colors: {
     textPrimary: "#24292e",
     textSecondary: "#586069",
-    primary: "#0366d6",
+    primary: "#0366d6"
   },
   fontSizes: {
     body: 14,
     subheading: 16
   },
   fonts: {
-    main: "System"
+    main: Platform.select({
+      android: "Roboto",
+      ios: "Arial",
+      default: "System"
+    })
   },
   fontWeights: {
     normal: "400",
