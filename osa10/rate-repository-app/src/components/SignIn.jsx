ff57d6a9d836ac3d4ac7e2a93e38c0ff8fb2fb7c
JesseK13241 <138243431+JesseK13241@users.noreply.github.com>
2024-08-04 17:56:30 +0300

    Exercise 10.9: validating the sign-in form

--- osa10/rate-repository-app/src/components/SignIn.jsx
+++ osa10/rate-repository-app/src/components/SignIn.jsx
@@ -1,27 +1,41 @@
+// if a field has an error, the TextInput component sets the TextInput component's error prop as true. Use the value of the error prop to attach conditional border color to the TextInput component.
+
 import React from "react";
 import View from "./View";
-import { TextInput, Button, StyleSheet } from "react-native";
+import { Text, TextInput, Button, StyleSheet } from "react-native";
 import { useFormik } from "formik";
 import theme from "../theme";
+import * as yup from "yup";
 
 const styles = StyleSheet.create({
   input: {
     width: "auto",
     height: 70,
     color: theme.colors.textSecondary,
-    borderColor: theme.colors.textSecondary,
     borderWidth: 1,
     padding: 20,
     marginTop: 10
+  },
+  inputError: {
+    borderColor: "red"
+  },
+  inputNormal: {
+    borderColor: theme.colors.textSecondary
   },
   signInButton: {
     marginTop: 10
   }
 });
 
+const validationSchema = yup.object().shape({
+  username: yup.string().required("Username is required"),
+  password: yup.string().required("Password is required")
+});
+
 const SignIn = () => {
   const formik = useFormik({
     initialValues: { username: "", password: "" },
+    validationSchema,
     onSubmit: values => {
       console.log(values);
     }
@@ -29,18 +43,38 @@ const SignIn = () => {
   return (
     <View container>
       <TextInput
-        style={styles.input}
+        style={[
+          styles.input,
+          formik.touched.username && formik.errors.username
+            ? styles.inputError
+            : styles.inputNormal
+        ]}
         placeholder="Username"
         onChangeText={formik.handleChange("username")}
         value={formik.values.username}
       />
+
+      {formik.touched.username && formik.errors.username && (
+        <Text style={{ color: "red" }}>{formik.errors.username}</Text>
+      )}
+
       <TextInput
-        style={styles.input}
+        style={[
+          styles.input,
+          formik.touched.password && formik.errors.password
+            ? styles.inputError
+            : styles.inputNormal
+        ]}
         placeholder="Password"
         onChangeText={formik.handleChange("password")}
         value={formik.values.password}
         secureTextEntry
       />
+
+      {formik.touched.password && formik.errors.password && (
+        <Text style={{ color: "red" }}>{formik.errors.password}</Text>
+      )}
+
       <View style={styles.signInButton}>
         <Button
           onPress={formik.handleSubmit}
