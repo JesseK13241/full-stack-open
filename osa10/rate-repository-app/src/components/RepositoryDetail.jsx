5bcf64f913931e75cbf820755cf1039ae46a9845
JesseK13241 <138243431+JesseK13241@users.noreply.github.com>
2024-08-10 19:50:41 +0300

    Exercise 10.20: repository's review list

--- osa10/rate-repository-app/src/components/RepositoryDetail.jsx
+++ osa10/rate-repository-app/src/components/RepositoryDetail.jsx
@@ -1,7 +1,53 @@
-import { Text, View } from "react-native";
+import { FlatList, StyleSheet } from "react-native";
 import { useParams } from "react-router-dom";
 import useRepository from "../hooks/useRepository";
+import theme from "../theme";
 import RepositoryItem from "./RepositoryItem";
+import Text from "./Text";
+import View from "./View";
+
+const styles = StyleSheet.create({
+  separator: {
+    backgroundColor: "#e1e4e8",
+    height: 10
+  },
+  rating: {
+    borderColor: theme.colors.primary,
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    borderWidth: 2,
+    alignItems: "center",
+    paddingTop: 8
+  },
+  reviewText: {
+    paddingTop: 10,
+    paddingRight: 50
+  }
+});
+
+const ReviewItem = ({ review }) => {
+  return (
+    <View container horizontal>
+      <View style={styles.rating}>
+        <Text>{review.rating}</Text>
+      </View>
+      <View>
+        <View>
+          <Text bold>{review.user.username}</Text>
+        </View>
+        <View>
+          <Text>{review.createdAt.split("T")[0]}</Text>
+        </View>
+        <View style={styles.reviewText}>
+          <Text>{review.text}</Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const ItemSeparator = () => <View style={styles.separator} />;
 
 const RepositoryDetail = () => {
   const { id } = useParams();
@@ -10,10 +56,22 @@ const RepositoryDetail = () => {
   if (loading) return <Text>Loading...</Text>;
   if (error) return <Text>Error: {error.message}</Text>;
 
+  const reviews =
+    data.repository && data.repository.reviews.edges.map(edge => edge.node);
+
   return (
-    <View>
-      {data && data.repository && <RepositoryItem item={data.repository} />}
-    </View>
+    <FlatList
+      data={reviews}
+      renderItem={({ item }) => <ReviewItem review={item} />}
+      keyExtractor={({ id }) => id}
+      ItemSeparatorComponent={ItemSeparator}
+      ListHeaderComponent={() => (
+        <>
+          <RepositoryItem item={data.repository} />
+          <ItemSeparator />
+        </>
+      )}
+    />
   );
 };
 
