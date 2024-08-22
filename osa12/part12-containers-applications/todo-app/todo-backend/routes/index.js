6726123d4ecbe94e936c50676e07a5e77963cb73
JesseK13241 <138243431+JesseK13241@users.noreply.github.com>
2024-08-22 17:25:38 +0300

    Exercise 12.10:

--- osa12/part12-containers-applications/todo-app/todo-backend/routes/index.js
+++ osa12/part12-containers-applications/todo-app/todo-backend/routes/index.js
@@ -1,18 +1,24 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const redis = require('../redis')
-const configs = require('../util/config')
+const redis = require("../redis");
+const configs = require("../util/config");
 
-let visits = 0
+const VISITS_KEY = "visit_count";
 
-/* GET index data. */
-router.get('/', async (req, res) => {
-  visits++
+router.get("/", async (req, res) => {
+  let visits = (await redis.getAsync(VISITS_KEY)) || 0;
+  visits = parseInt(visits) + 1;
+  await redis.setAsync(VISITS_KEY, visits);
 
   res.send({
     ...configs,
     visits
   });
+});
+
+router.get("/statistics", async (req, res) => {
+  const statistics = await redis.getAsync(VISITS_KEY);
+  res.json(statistics);
 });
 
 module.exports = router;
