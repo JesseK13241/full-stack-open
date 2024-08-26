60232207e31b5935e4ffbe94e07f01cebcdbdbfb
JesseK13241 <138243431+JesseK13241@users.noreply.github.com>
2024-08-26 14:37:01 +0300

    Tehtävä 13.5.

---
+++ osa13/postgreSQL/backend/controllers/blogs.js
@@ -0,0 +1,58 @@
+const { Blog } = require("../models");
+const express = require("express");
+const blogRouter = express.Router();
+
+const blogFinder = async (req, res, next) => {
+  req.blog = await Blog.findByPk(req.params.id);
+  console.log("Blog found", JSON.stringify(req.blog));
+  next();
+};
+
+blogRouter.get("/", async (req, res) => {
+  const blogs = await Blog.findAll();
+  console.log("Fetching all blogs");
+  console.log(JSON.stringify(blogs));
+  res.json(blogs);
+});
+
+blogRouter.post("/", async (req, res) => {
+  try {
+    console.log("Creating blog", JSON.stringify(req.body));
+    const blog = await Blog.create(req.body);
+    console.log("Created blog", JSON.stringify(blog));
+    return res.json(blog);
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
+});
+
+blogRouter.get("/:id", blogFinder, async (req, res) => {
+  console.log("Fetching blog by id", req.params.id);
+  if (req.blog) {
+    res.json(req.blog);
+  } else {
+    res.status(404).end();
+  }
+});
+
+blogRouter.put("/:id", blogFinder, async (req, res) => {
+  if (req.blog) {
+    await req.blog.save();
+    console.log("Updated blog", JSON.stringify(req.blog));
+    res.json(req.blog);
+  } else {
+    res.status(404).end();
+  }
+});
+
+blogRouter.delete("/:id", blogFinder, async (req, res) => {
+  if (req.blog) {
+    await req.blog.destroy();
+    console.log("Deleted blog", req.params.id);
+    res.status(204).end();
+  } else {
+    res.status(404).end();
+  }
+});
+
+module.exports = blogRouter;
--- osa13/postgreSQL/backend/index.js
+++ osa13/postgreSQL/backend/index.js
@@ -1,100 +1,19 @@
-require("dotenv").config();
-
-const { Sequelize, Model, DataTypes } = require("sequelize");
 const express = require("express");
 const app = express();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL);
+const { PORT } = require("./util/config");
+const { connectToDatabase } = require("./util/db");
 
-class Blog extends Model {}
+const blogRouter = require("./controllers/blogs");
 
-Blog.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    author: {
-      type: DataTypes.TEXT
-    },
-    url: {
-      type: DataTypes.TEXT,
-      allowNull: false
-    },
-    title: {
-      type: DataTypes.TEXT,
-      allowNull: false
-    },
-    likes: {
-      type: DataTypes.INTEGER
-    }
-  },
-  {
-    sequelize,
-    underscored: true,
-    timestamps: false,
-    modelName: "blog"
-  }
-);
+app.use(express.json());
+app.use("/api/blogs", blogRouter);
 
-Blog.sync();
+const start = async () => {
+  await connectToDatabase();
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+};
 
-app.get("/api/blogs", async (req, res) => {
-  const blogs = await Blog.findAll();
-  console.log("Fetching all blogs");
-  console.log(JSON.stringify(blogs));
-  res.json(blogs);
-});
-
-app.get("/api/blogs/:id", async (req, res) => {
-  const blog = await Blog.findByPk(req.params.id);
-  console.log("Fetching blog by id", req.params.id);
-  if (blog) {
-    console.log("Blog found", JSON.stringify(blog));
-    res.json(blog);
-  } else {
-    res.status(404).end();
-  }
-});
-
-app.post("/api/blogs", async (req, res) => {
-  try {
-    console.log("Creating blog", JSON.stringify(req.body));
-    const blog = await Blog.create(req.body);
-    console.log("Created blog", JSON.stringify(blog));
-    return res.json(blog);
-  } catch (error) {
-    return res.status(400).json({ error });
-  }
-});
-
-app.put("/api/blogs/:id", async (req, res) => {
-  console.log("Updating blog", req.params.id, JSON.stringify(req.body));
-  const blog = await Blog.findByPk(req.params.id);
-  if (blog) {
-    await blog.save();
-    console.log("Updated blog", JSON.stringify(blog));
-    res.json(blog);
-  } else {
-    console.log("Blog not found", req.params.id);
-    res.status(404).end();
-  }
-});
-
-app.delete("/api/blogs/:id", async (req, res) => {
-  const blog = await Blog.findByPk(req.params.id);
-  if (blog) {
-    await blog.destroy();
-    console.log("Deleted blog", req.params.id);
-    res.status(204).end();
-  } else {
-    console.log("Blog not found", req.params.id);
-    res.status(404).end();
-  }
-});
-
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+start();
