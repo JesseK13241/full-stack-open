d911d1468e36a5bba8e778eafc10fedc494df44d
JesseK13241 <138243431+JesseK13241@users.noreply.github.com>
2024-08-26 14:17:21 +0300

    Tehtävä 13.4.

--- osa13/postgreSQL/backend/index.js
+++ osa13/postgreSQL/backend/index.js
@@ -38,17 +38,59 @@ Blog.init(
   }
 );
 
+Blog.sync();
+
 app.get("/api/blogs", async (req, res) => {
   const blogs = await Blog.findAll();
+  console.log("Fetching all blogs");
+  console.log(JSON.stringify(blogs));
   res.json(blogs);
+});
+
+app.get("/api/blogs/:id", async (req, res) => {
+  const blog = await Blog.findByPk(req.params.id);
+  console.log("Fetching blog by id", req.params.id);
+  if (blog) {
+    console.log("Blog found", JSON.stringify(blog));
+    res.json(blog);
+  } else {
+    res.status(404).end();
+  }
 });
 
 app.post("/api/blogs", async (req, res) => {
   try {
+    console.log("Creating blog", JSON.stringify(req.body));
     const blog = await Blog.create(req.body);
+    console.log("Created blog", JSON.stringify(blog));
     return res.json(blog);
   } catch (error) {
     return res.status(400).json({ error });
+  }
+});
+
+app.put("/api/blogs/:id", async (req, res) => {
+  console.log("Updating blog", req.params.id, JSON.stringify(req.body));
+  const blog = await Blog.findByPk(req.params.id);
+  if (blog) {
+    await blog.save();
+    console.log("Updated blog", JSON.stringify(blog));
+    res.json(blog);
+  } else {
+    console.log("Blog not found", req.params.id);
+    res.status(404).end();
+  }
+});
+
+app.delete("/api/blogs/:id", async (req, res) => {
+  const blog = await Blog.findByPk(req.params.id);
+  if (blog) {
+    await blog.destroy();
+    console.log("Deleted blog", req.params.id);
+    res.status(204).end();
+  } else {
+    console.log("Blog not found", req.params.id);
+    res.status(404).end();
   }
 });
 
