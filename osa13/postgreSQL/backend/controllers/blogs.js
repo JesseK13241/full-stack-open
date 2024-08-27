f8bf54c9d6e1d87f3f2ede96f662a6dc89524e25
JesseK13241 <138243431+JesseK13241@users.noreply.github.com>
2024-08-27 16:27:47 +0300

    Tehtävä 13.14.

--- osa13/postgreSQL/backend/controllers/blogs.js
+++ osa13/postgreSQL/backend/controllers/blogs.js
@@ -52,9 +52,14 @@ blogRouter.get("/", async (req, res) => {
       attributes: ["name"]
     },
     where: {
-      title: {
-        [Op.iLike]: `%${req.query.search ? req.query.search : ""}%`
-      }
+      [Op.or]: [
+        { title: {
+          [Op.iLike]: `%${req.query.search ? req.query.search : ""}%`
+        }},
+        { author: {
+          [Op.iLike]: `%${req.query.search ? req.query.search : ""}%`
+        }}
+      ]
     }
   });
   res.json(blogs);
