19f80a812e7b3cb4eda9a1345cb319f90aa5930b
JesseK13241 <138243431+JesseK13241@users.noreply.github.com>
2024-08-27 17:05:21 +0300

    cleanup

--- osa13/postgreSQL/backend/controllers/authors.js
+++ osa13/postgreSQL/backend/controllers/authors.js
@@ -6,37 +6,25 @@ const { Sequelize } = require("sequelize");
 const sequelize = new Sequelize(process.env.DATABASE_URL);
 
 authorRouter.get("/", async (req, res) => {
-    
-    // {
-    //     author: "new author",
-    //     blogs: "1",
-    //     likes: "100"
-    // };
+  // {
+  //     author: "new author",
+  //     blogs: "1",
+  //     likes: "100"
+  // };
 
-  try {
-    const authors = await Blog.findAll({
-      attributes: [
-        // columns to include
-        "author",
-        [sequelize.fn("COUNT", sequelize.col("id")), "blogs"],
-        [sequelize.fn("SUM", sequelize.col("likes")), "likes"]
-      ],
-      group: ["author"],
-      order: [[sequelize.fn("SUM", sequelize.col("likes")), "DESC"]],
-      raw: true
-    });
+  const authors = await Blog.findAll({
+    attributes: [
+      // columns to include
+      "author",
+      [sequelize.fn("COUNT", sequelize.col("id")), "blogs"],
+      [sequelize.fn("SUM", sequelize.col("likes")), "likes"]
+    ],
+    group: ["author"],
+    order: [[sequelize.fn("SUM", sequelize.col("likes")), "DESC"]],
+    raw: true
+  });
 
-    const formattedAuthors = authors.map(author => ({
-      author: author.author,
-      blogs: author.blogs.toString(),
-      likes: author.likes.toString()
-    }));
-
-    res.json(formattedAuthors);
-  } catch (error) {
-    console.error("Error fetching authors:", error);
-    res.status(500).json({ error: "Internal server error" });
-  }
+  res.json(authors);
 });
 
 module.exports = authorRouter;
