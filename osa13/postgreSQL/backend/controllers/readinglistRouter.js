0c95bc931382c44298af04d899c41b1fee55861b
JesseK13241 <138243431+JesseK13241@users.noreply.github.com>
2024-08-27 19:22:12 +0300

    Tehtävä 13.22.

--- osa13/postgreSQL/backend/controllers/readinglistRouter.js
+++ osa13/postgreSQL/backend/controllers/readinglistRouter.js
@@ -2,13 +2,26 @@ const { Readlist } = require("../models");
 
 const express = require("express");
 const readinglistRouter = express.Router();
+const tokenExtractor = require("../util/tokenExtractor.js");
 
 readinglistRouter.post("/", async (req, res) => {
-    const readlistEntry = await Readlist.create({
-      ...req.body,
-      readStatus: false
-    });
-    res.json(readlistEntry);
+  const readlistEntry = await Readlist.create({
+    ...req.body,
+    readStatus: false
+  });
+  res.json(readlistEntry);
+});
+
+readinglistRouter.put("/:id", tokenExtractor, async (req, res) => {
+  const readlistEntry = await Readlist.findByPk(req.params.id);
+  if (req.decodedToken.id !== readlistEntry.dataValues.userId) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+  readlistEntry.set({ readStatus: req.body.readStatus });
+  await readlistEntry.save();
+
+  // SequelizeValidationError notNull Violation: readlist.readStatus cannot be null
+  res.json(readlistEntry);
 });
 
 module.exports = readinglistRouter;
