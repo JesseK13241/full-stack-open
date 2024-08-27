58bbbc90469e357679cd64c25ef1cce3e5c77497
JesseK13241 <138243431+JesseK13241@users.noreply.github.com>
2024-08-27 19:09:21 +0300

    Tehtävä 13.21.

--- osa13/postgreSQL/backend/controllers/users.js
+++ osa13/postgreSQL/backend/controllers/users.js
@@ -35,7 +35,7 @@ router.get("/:id", async (req, res) => {
         model: Blog,
         as: "markedBlogs",
         attributes: { exclude: ["userId"] },
-        through: { attributes: [] }
+        through: { attributes: ["readStatus", "id"] }
       }
     ]
   });
