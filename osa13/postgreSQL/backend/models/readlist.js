06b72537398e6e2ee31dd6e80b52746a4a12d615
JesseK13241 <138243431+JesseK13241@users.noreply.github.com>
2024-08-27 18:43:43 +0300

    Tehtävä 13.19.

---
+++ osa13/postgreSQL/backend/models/readlist.js
@@ -0,0 +1,37 @@
+const { Model, DataTypes } = require("sequelize");
+
+const { sequelize } = require("../util/db");
+
+class Readlist extends Model {}
+
+Readlist.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: { model: "users", key: "id" }
+    },
+    blogId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: { model: "blogs", key: "id" }
+    },
+    readStatus: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false
+    }
+  },
+  {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: "readlist"
+  }
+);
+
+module.exports = Readlist;
