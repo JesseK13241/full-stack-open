41bd5b1c338c5dbcbabaeda458d3c1cfe789ead5
JesseK13241 <138243431+JesseK13241@users.noreply.github.com>
2024-08-27 17:06:39 +0300

    cleanup2

--- osa13/postgreSQL/backend/controllers/authors.js
+++ osa13/postgreSQL/backend/controllers/authors.js
@@ -6,22 +6,19 @@ const { Sequelize } = require("sequelize");
 const sequelize = new Sequelize(process.env.DATABASE_URL);
 
 authorRouter.get("/", async (req, res) => {
-  // {
-  //     author: "new author",
-  //     blogs: "1",
-  //     likes: "100"
-  // };
+  // Return all authors, sorted by likes
+  // {author: "new author",
+  // blogs: "1",
+  // likes: "100"}
 
   const authors = await Blog.findAll({
     attributes: [
-      // columns to include
       "author",
       [sequelize.fn("COUNT", sequelize.col("id")), "blogs"],
       [sequelize.fn("SUM", sequelize.col("likes")), "likes"]
     ],
     group: ["author"],
-    order: [[sequelize.fn("SUM", sequelize.col("likes")), "DESC"]],
-    raw: true
+    order: [[sequelize.fn("SUM", sequelize.col("likes")), "DESC"]]
   });
 
   res.json(authors);
